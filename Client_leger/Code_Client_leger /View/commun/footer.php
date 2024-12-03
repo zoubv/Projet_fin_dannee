31c5df08a7912da0e5733afef8004eef4a35573d
Zoubairou Diallo <zoubairoudiallo729@gmail.com>
2024-12-03 22:11:13 +0100

    maj
    mise a jour avec les pages fonctionnel

--- Client_leger/Code_Client_leger /View/commun/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="bg-light text-center text-lg-start mt-5">
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2025 Média Orientation | Tous droits réservés.
-    </div>
-</footer>
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
